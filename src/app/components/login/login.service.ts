05720f7638be57589d9eff24602223f0ebf1b469
Mai Trang <maitrangsunny79@gmail.com>
2019-11-11 07:45:46 +0700

    add files

---
+++ src/app/components/login/login.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+
+@Injectable()
+
+export class LoginService {
+    
+    constructor(private http: HttpClient) { }
+    sendPost(value){
+        const url = 'http://localhost:3000/signin';
+        const headers = new HttpHeaders({'Content-Type': 'application/json'});
+        const body =  JSON.stringify(value);
+        return this.http.post(url, body, { headers});
+        // return this.http.post(url, body, { headers}).subscribe((response: Response)=>{
+        //     console.log(response);
+        //     // return response;
+        // });
+
+    }
+
+}
